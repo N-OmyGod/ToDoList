c7e1c6e73115a9c419acac215842bdf311453a97
Nasiba Uzdenova <uzdennasub@gmail.com>
2021-07-14 19:15:37 +0300

    debag vers

--- main.js
+++ main.js
@@ -13,18 +13,19 @@ $(document).on('click', '#add-task-btn',function() {
    let value = $('#description-task').val();
    Tasks.push({value:value, completed:false, id: guid()});
    render();
+ 
 })
 
  function render(){
     let string=`` 
      Tasks.forEach((task)=>{
         string+=`
-        <div> 
-        <input type="checkbox" class="complete-checkbox" id=${task.id}/>
+        <div class="todo-item"> 
+        <input type="checkbox" class="complete-checkbox" id="${task.id}"/>
         <span>
         ${task.value}
         </span>
-        <input type="button" id=${task.id} value="delete"/>
+        <input type="button" class="delete-button" id="${task.id}" value="delete"/>
         </div>
        `
      })
@@ -33,16 +34,24 @@ $(document).on('click', '#add-task-btn',function() {
 
  
  $(document).on('click', '.complete-checkbox',function() {
-    const id=$(this).attr('completed');
-    this.completed=!this.completed
-     for(let i=0; i<Tasks.length; i++){
-         if(this.id==Tasks[i].id){
-            Tasks[i].completed= !Tasks[i].completed;
-            console.log(Tasks[i].completed);
-         }
-     }
-
+    const id=$(this).attr('id');
+Tasks.forEach((task)=>{
+if (task.id===id){
+task.completed=!task.completed
+}
+})
 console.log(Tasks);
-  
+
+    
  })
+ 
+ $(document).on('click', '.delete-button',function() {
+    const id=$(this).attr('id');
+    const ind=Tasks.findIndex(task=>task.id===id);
+    Tasks.splice(ind,1);
+
+
+    render();
+ })
+ 
  
