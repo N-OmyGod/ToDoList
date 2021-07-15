6affd3cd5705d1a0604a5331959817b86204edba
Nasiba Uzdenova <uzdennasub@gmail.com>
2021-07-15 16:26:56 +0300

    so

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let Tasks=[];
-
+let cl=0;
+let clun=0;
 let guid = () => {
     let s4 = () => {
         return Math.floor((1 + Math.random()) * 0x10000)
@@ -16,7 +17,44 @@ $(document).on('click', '#add-task-btn',function() {
    render();
  
 })
+function  Completedrender(){
+   let string=`` 
+   Tasks.forEach((task)=>{
 
+      let isGrey= task.completed ? 'grey' : null;
+      let isChecked=task.completed ? 'checked' : null;
+      if(isChecked!=null){
+      string+=`
+      <div class="todo-item  ${isGrey}" id="todo-${task.id}"> 
+      <input type="checkbox" class="complete-checkbox" id="${task.id}" ${isChecked}/>
+      <span>
+      ${task.value}
+      </span>
+      <input type="button" class="delete-button" id="${task.id}" value="delete"/>
+      </div>
+     `}
+   })
+$('.todos-wrapper').html(string);
+
+}
+function UnCompletedrender(){
+   let string=`` 
+   Tasks.forEach((task)=>{
+      let isGrey= task.completed ? 'grey' : null;
+      let isChecked=task.completed ? 'checked' : null;
+      if(isChecked===null){
+      string+=`
+      <div class="todo-item  ${isGrey}" id="todo-${task.id}"> 
+      <input type="checkbox" class="complete-checkbox" id="${task.id}" ${isChecked}/>
+      <span>
+      ${task.value}
+      </span>
+      <input type="button" class="delete-button" id="${task.id}" value="delete"/>
+      </div>
+     `}
+   })
+$('.todos-wrapper').html(string);
+}
  function render(){
 
     let string=`` 
@@ -52,18 +90,41 @@ task.completed=!task.completed
    }
 }
 })
+
+
 console.log(Tasks);
 
     
  })
  
+
+
  $(document).on('click', '.delete-button',function() {
     const id=$(this).attr('id');
     const ind=Tasks.findIndex(task=>task.id===id);
     Tasks.splice(ind,1);
-
-
     render();
  })
  
- 
+
+
+
+
+
+
+ $(document).on('click', '#all-btn',function() {
+   render();
+})
+$(document).on('click', '#completed-btn',function() {
+   $( '#completed-btn').addClass('active');
+   $( '#uncompleted-btn').removeClass('active');
+   $( '#all-btn').removeClass('active');
+   Completedrender();  
+})
+$(document).on('click', '#uncompleted-btn',function() {
+   $( '#uncompleted-btn').addClass('active');
+   $( '#completed-btn').removeClass('active');
+   $( '#all-btn').removeClass('active');
+   UnCompletedrender();
+
+})
